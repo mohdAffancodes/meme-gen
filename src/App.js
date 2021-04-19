fa81a99047f4e27c9ba652857fcdf9feec7243bc
mohdAffancodes <78096552+mohdAffancodes@users.noreply.github.com>
2021-04-19 13:50:21 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./comps/Header/Header";
+import MemeGenerator from "./comps/MemeGenerator";
+
+function App() {
+   return (
+      <div>
+         <Header />
+         <MemeGenerator />
+         <p className="extra-space"></p>
+      </div>
+   );
+}
+
+export default App;
